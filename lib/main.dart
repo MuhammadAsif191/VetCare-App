986cc9c7fffa25bb42e85215059d34f8a8cecdb5
muhammadshiraz492 <muhammadshiraz492@gmail.com>
2022-09-01 01:21:02 +0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'UI/Home.dart';
 import 'package:flutter/services.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 void main() async {
+  IO.Socket socket = IO.io('https://mix-chat-1.herokuapp.com');
+// socket.on('connect',void Function(){ print("data")});
+  socket.on('connect', (socket) => print(socket.id));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
